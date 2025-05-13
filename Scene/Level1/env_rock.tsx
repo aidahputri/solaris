0ad34dcaeb8cdad836ae472425c519f4a1e3d44b
Aidah Novallia Putri <aidahptr15@gmail.com>
2025-05-13 22:24:36 +0700

    feat: init level 1 tilemap

---
+++ Scene/Level1/env_rock.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="env_rock" tilewidth="16" tileheight="16" tilecount="900" columns="30">
+ <image source="env_rock.png" width="480" height="480"/>
+</tileset>

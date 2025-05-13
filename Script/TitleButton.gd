1ae616bb8d3a13fe389c5e4615166754879be463
Aidah Novallia Putri <aidahptr15@gmail.com>
2025-05-13 20:43:09 +0700

    feat: add prologue

--- Script/TitleButton.gd
+++ Script/TitleButton.gd
@@ -4,7 +4,7 @@ func _on_pressed():
 	call_deferred("start_scene")
 
 func start_scene():
-	get_tree().change_scene_to_file(str("res://Scene/Level1.tscn"))
+	get_tree().change_scene_to_file(str("res://Scene/Prologue.tscn"))
 
 func _on_exit_pressed():
 	call_deferred("exit_game")
